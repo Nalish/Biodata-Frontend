4e8fe9c83c749a1a708954aba0181c46d911dca1
JoshMith <150962666+JoshMith@users.noreply.github.com>
2025-05-10 16:21:04 +0300

    deploy2

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -3,33 +3,28 @@ const exec = require('@actions/exec') // Helps with executing commands
 
 async function run() {
     try {
-        // Get inputs
         const vercelToken = core.getInput('vercel-token', { required: true })
         const distFolder = core.getInput('dist-folder', { required: true })
 
-        // Deploy to Vercel
+        let output = ''
+        const options = {
+            listeners: {
+                stdout: (data) => {
+                    output += data.toString()
+                }
+            }
+        }
+
+        // First deployment
         await exec.exec(`npx vercel --prod --yes --token ${vercelToken} --cwd ${distFolder}`, [], options)
 
-        // Set output for deployment URL
-        const deployUrl = await getDeployUrl(distFolder, vercelToken)
+        // Extract deploy URL from output
+        const match = output.match(/https?:\/\/[^\s]+/g)
+        const deployUrl = match ? match[0] : ''
         core.setOutput('deploy-url', deployUrl)
     } catch (error) {
         core.setFailed(error.message)
     }
-}
-
-async function getDeployUrl(distFolder, vercelToken) {
-    let output = ''
-    const options = {
-        listeners: {
-            stdout: (data) => {
-                output += data.toString()
-            }
-        }
-    }
-    await exec.exec(`npx vercel --prod --token ${vercelToken} --cwd ${distFolder}`, [], options)
-    const match = output.match(/https?:\/\/[^\s]+/g)
-    return match ? match[0] : ''
 }
 
 run()
